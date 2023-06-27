a7eff6c358e0241c3f353cefc7b6858dcd20872a
hui1110 <hui1110rui@163.com>
2023-06-27 11:22:31 +0800

    first commit

---
+++ src/main/java/AzureSpringAdmin/AzureSpringAdminApplication.java
@@ -0,0 +1,13 @@
+package AzureSpringAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSpringAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureSpringAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/AzureSpringAdmin/AzureSpringAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package AzureSpringAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSpringAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
