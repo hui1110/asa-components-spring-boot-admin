19d4b74a1d0e163c67a4e884aa55ed8db4e9af0a
hui1110 <hui1110rui@163.com>
2023-07-17 13:59:15 +0800

    update java version

--- src/main/java/com/azure/spring/ASAComponentSpringBootAdminApplication.java
+++ src/main/java/com/azure/spring/ASAComponentSpringBootAdminApplication.java
@@ -1,11 +1,9 @@
 package com.azure.spring;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 @EnableAdminServer
@@ -15,16 +13,8 @@ public class ASAComponentSpringBootAdminApplication implements CommandLineRunner
         SpringApplication.run(ASAComponentSpringBootAdminApplication.class, args);
     }
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
     @Override
-    public void run(String... args) {
-        String sql = "INSERT INTO users (fullname, email, password) VALUES (?, ?, ?)";
+    public void run(String... args) throws Exception {
 
-        int result = jdbcTemplate.update(sql, "Ravi Kumar", "ravi.kumar@gmail.com", "ravi2021");
-
-        if (result > 0) {
-            System.out.println("A new row has been inserted.");
-        }
     }
 }
